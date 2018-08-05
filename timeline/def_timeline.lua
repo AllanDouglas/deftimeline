2e3bae69fe451e4d4f6177285310f2f2b99aee35
allan douglas <allandouglas@live.com>
2018-08-05 13:55:46 -0300

    animator functions

--- timeline/def_timeline.lua
+++ timeline/def_timeline.lua
@@ -1,10 +1,38 @@
 local M = {}
 
-function M.new()
-    local this = {
-        functions = {}
-    }
+local function randomString(length)
+    local res = ""
+    for i = 1, length do
+        res = res .. string.char(math.random(97, 122))
+    end
+    return res
+end
 
+animator = {
+    index = 0,
+    animations = {},
+    labels = {},
+    animate = function()
+        animator.index = animator.index + 1
+
+        local callback = nil
+
+        local animation = animator.animations[animator.labels[animator.index]]
+        go.animate(
+            animation.hash,
+            animation.property,
+            animation.playback,
+            animation.to,
+            animation.easing,
+            animation.duration,
+            animation.delay,
+            callback
+        )
+    end
+}
+
+function M.new()
+    this = {}
     local set_position = function(hash, property, to)
         go.set_position(to, hash)
         return this
@@ -15,27 +43,55 @@ function M.new()
         return this
     end
 
-    this.to_position = function(url, property, playback, to, easing, duration, delay)
-        return this
+    this.play = function()
+        animator.animate()
     end
 
-    this.from_position = function()
-        return this
+    this.debug = function()
+        pprint(animator.animations)
     end
 
-    this.from_to_position = function()
-        return this
-    end
+    this.add = function(url, property, playback, to, easing, duration, label, delay)
+        label = label or randomString(5)
 
-    this.to_rotation = function()
-        return this
-    end
+        local func = {
+            property = property,
+            to = to,
+            label = label,
+            hash = url,
+            playback = playback or go.PLAYBACK_ONCE_FORWARD,
+            easing = easing or go.EASING_LINEAR,
+            duration = duration or 0,
+            delay = delay or 0
+        }
 
-    this.from_rotation = function()
-        return this
-    end
+        local animation = nil
 
-    this.fromToRotation = function()
+        if #animator.animations > 0 then
+            function get_animation(animations, i, l)
+                if i > #animations then
+                    return nil
+                elseif animations[i].label == l then
+                    return animations[i]
+                end
+                return get_animation(animations, i + 1, l)
+            end
+
+            animation = get_animation(animator.animations, 1, label)
+        end
+
+        if animation == nil then
+            animation = {
+                animations = {
+                    func
+                },
+                label = label
+            }
+        else
+            table.insert(a.animations, func)
+        end
+
+        table.insert(animator.animations, animation)
         return this
     end
 
